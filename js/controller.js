ebb1ca7293833741bb62bb54178cc2d957bb5485
TohaDoku <143796346+TohaDoku@users.noreply.github.com>
2024-03-23 09:38:45 +0300

    Базовая настройка проекта

---
+++ js/controller.js
@@ -0,0 +1,17 @@
+import * as Model from "./model.js";
+import programs from './view/radioPrograms.js'
+
+window.onload = function () {
+	const getData = Model.getData;
+
+	// Init programs
+    programs(getData);
+
+    document.addEventListener('updateForm', (e) => {
+        console.log('FIRED!!!!');
+        console.log(e.detail);
+
+        Model.setData(e.detail);
+
+    })
+}
---
+++ js/model.js
@@ -0,0 +1,26 @@
+let data = {
+	selectedProgram: 0.1,
+	programs: {
+		base: 0.1,
+		it: 0.047,
+		gov: 0.067,
+		zero: 0.12,
+	},
+};
+
+function getData() {
+    return {...data}
+}
+
+function setData(newData) {
+    console.log('New data', newData);
+
+    data = {
+        ...data,
+        ...newData
+    }
+
+    console.log('Updated data', data);
+}
+
+export { getData, setData };
